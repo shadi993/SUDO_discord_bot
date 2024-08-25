269ef30aa88d60f8f4f5c5a1128a949fa422805c
Robin Degen <robindegen@gmail.com>
2024-08-25 18:02:03 +0200

    Move the entirety of init commands to the init.mjs out of the bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,52 +1,31 @@
 import { config } from 'dotenv';
 import { InitCommands } from './commands/init.mjs';
-import {Client, GatewayIntentBits, Events, Collection} from 'discord.js';
+import { Client, GatewayIntentBits, Events } from 'discord.js';
 
 config();
 
-const client = new Client({ intents:
-    GatewayIntentBits.Guilds |
-    GatewayIntentBits.GuildMembers |
-    GatewayIntentBits.GuildMessages |
-    GatewayIntentBits.GuildMessageReactions |
-    GatewayIntentBits.GuildMembers |
-    GatewayIntentBits.GuildEmojisAndStickers |
-    GatewayIntentBits.DirectMessages |
-    GatewayIntentBits.DirectMessageReactions |
-    GatewayIntentBits.MessageContent
+const client = new Client({
+    intents:
+        GatewayIntentBits.Guilds |
+        GatewayIntentBits.GuildMembers |
+        GatewayIntentBits.GuildMessages |
+        GatewayIntentBits.GuildMessageReactions |
+        GatewayIntentBits.GuildMembers |
+        GatewayIntentBits.GuildEmojisAndStickers |
+        GatewayIntentBits.DirectMessages |
+        GatewayIntentBits.DirectMessageReactions |
+        GatewayIntentBits.MessageContent
 });
 
-client.commands = new Collection();
-InitCommands(client.commands);
+InitCommands(client);
 
-client.once(Events.ClientReady,() => {
+client.once(Events.ClientReady, () => {
     console.log(`${client.user.username} has logged in.`);
-});
-
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
 });
 
 client.on(Events.MessageCreate, async (message) => {
     console.log(`[${message.author.tag}]: ${message.content}`);
-    if (message.content === 'hello'){
+    if (message.content === 'hello') {
         message.reply('Hello');
     }
 });
--- src/commands/init.mjs
+++ src/commands/init.mjs
@@ -2,8 +2,12 @@ import * as fs from 'node:fs';
 import * as path from 'node:path';
 import * as url from 'node:url';
 
-export const InitCommands = async (commands) => {
+import { Events, Collection } from 'discord.js';
+
+export const InitCommands = async (client) => {
     console.log('Initializing commands...');
+
+    client.commands = new Collection();
 
     const currentDirectory = path.dirname(url.fileURLToPath(import.meta.url));
     const commandFolders = fs.readdirSync(currentDirectory);
@@ -19,12 +23,33 @@ export const InitCommands = async (commands) => {
 
             if ('data' in command && 'execute' in command) {
                 console.log(`Registering command ${command.data.name}`);
-                commands.set(command.data.name, command);
+                client.commands.set(command.data.name, command);
             } else {
                 console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
             }
         }
     }
+
+    client.on(Events.InteractionCreate, async interaction => {
+        if (!interaction.isChatInputCommand()) return;
+        const command = interaction.client.commands.get(interaction.commandName);
+
+        if (!command) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
+        }
+
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            if (interaction.replied || interaction.deferred) {
+                await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+            } else {
+                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+            }
+        }
+    });
 
     console.log('Finished initializing commands.');
 }
