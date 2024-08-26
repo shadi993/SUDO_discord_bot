a25f1a06c004bba5e94d509fd09428dd0d882854
Async Exception <tijmenlodders@outlook.com>
2024-08-26 21:23:16 +0200

    Refactor roles.mjs

--- src/modules/roles.mjs
+++ src/modules/roles.mjs
@@ -6,106 +6,76 @@ import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } from 'disc
 import * as fs from 'node:fs';
 
 export const RolesModule = class {
+    #logger
+    #roles
+    #discordGuild
+    #discordChannels
+    #discordRoles
+
     constructor() {
-        this.logger = CreateLogger('RolesModule');
+        this.#logger = CreateLogger('RolesModule');
 
-        this.roles = JSON.parse(fs.readFileSync('roles.json'));
+        this.#roles = JSON.parse(fs.readFileSync('roles.json'));
 
-        if (!Array.isArray(this.roles)) {
-            this.logger.log('error', 'Roles must be an array.');
+        if (!Array.isArray(this.#roles)) {
+            this.#logger.log('error', 'Roles must be an array.');
             throw new Error('Roles must be an array.');
         }
     }
 
-    async onDiscordReady(guild, channels, roles) {
-        this.logger.log('info', 'Channels loaded. Roles module is loading...');
+    async #createOrUpdateMessage(role) {
+        // Find channel by name in the guild
+        this.#logger.log('debug', `Looking for channel: ${role.channel_name}`);
 
-        this.discordGuild = guild;
-        this.discordRoles = roles;
+        //find channel by id for future reference
+        //const channel = this.#discordChannels.find((channel) => channel.id === role.channel_id);
+        const channel = this.#discordChannels.find((channel) => channel.name === role.channel_name);
 
-        for (const role of this.roles) {
-
-            // Find channel by name in the guild
-            this.logger.log('debug', `Looking for channel: ${role.channel_name}`);
-            const channel = channels.find((channel) => channel.name === role.channel_name);
-
-            if (!channel) {
-                this.logger.log('error', `Channel not found: ${role.channel_name}`);
-                throw new Error(`Channel not found: ${role.channel_name}`);
-            }
-
-            channel.messages.fetch({ limit: 20 }).then(async (messages) => {
-                this.logger.log('debug', `Registering role: ${role.title}`);
-                this.logger.log('info', `Fetched ${messages.size} messages.`);
-
-                const message = messages.find((message) => message.author.id === process.env.DISCORD_CLIENT_ID &&
-                    message.embeds.length > 0 &&
-                    message.embeds[0].title === role.title);
-
-                // The message already exists. Edit it (TODO)
-                if (message) {
-                    this.logger.log('info', `Message found: ${role.title}. Updating...`);
-                    return;
-                }
-
-                if (!message) {
-                    this.logger.log('info', `Message not found: ${role.title}`);
-
-                    // Create the buttons
-                    var buttons = [];
-                    var optionIndex = 0;
-                    for (const option of role.options) {
-                        var button = new ButtonBuilder()
-                            .setCustomId('roleselection_' + role.id + '_' + optionIndex)
-                            .setStyle(this.getButtonStyleFromString(option.button_style))
-
-                        if (option.button_text) {
-                            button.setLabel(option.button_text)
-                        } else {
-                            button.setLabel('\u200B')
-                        }
-
-                        if (option.button_emoji) {
-                            button.setEmoji(option.button_emoji);
-                        }
-
-                        buttons.push(button);
-                        optionIndex++;
-                    }
-
-                    // Split up rows if there are more than 5 buttons
-                    var rows = [];
-                    while (buttons.length > 0) {
-                        rows.push(new ActionRowBuilder()
-                            .addComponents(buttons.splice(0, 5))
-                        );
-                    }
-
-                    const embed = new EmbedBuilder()
-                        .setTitle(role.title)
-                        .setDescription(" ")
-                        .setColor(0x0099FF);
-
-                    var fieldDescription = "";
-                    for (const option of role.options) {
-                        fieldDescription += option.description + "\n";
-                    }
-
-                    embed.addFields({
-                        name: '\u200B',
-                        value: fieldDescription,
-                        inline: false
-                    });
-
-                    await channel.send({
-                        embeds: [embed],
-                        components: rows
-                    });
-                }
-            });
+        if (!channel) {
+            this.#logger.log('error', `Channel not found: ${role.channel_name}`);
+            throw new Error(`Channel not found: ${role.channel_name}`);
         }
 
-        this.logger.log('info', 'Roles module is ready.');
+        let messages = await channel.messages.fetch({ limit: 20 });
+        this.#logger.log('debug', `Registering role: ${role.title}`);
+        this.#logger.log('info', `Fetched ${messages.size} messages.`);
+        
+        //Message id's should be stored in the database once this is ready. Searching on title is not unique.
+        let message = messages.find((message) => 
+            message.author.id === process.env.DISCORD_CLIENT_ID &&
+            message.embeds.length > 0 &&
+            message.embeds[0].title === role.title);
+
+        let embedGenerator = new ButtonEmbedGenerator(role);
+        let generatedMessage = {
+            embeds: [embedGenerator.getEmbed()],
+            components: embedGenerator.getRows()
+        };
+
+        if (message) {
+            //Update message if it already exists.
+            this.#logger.log('info', `Message found: ${role.title}. Updating...`);
+            message.edit(generatedMessage);
+        }
+        else
+        {
+            //Create new message if it doesnt exist.
+            this.#logger.log('info', `Message not found: ${role.title}`);
+            await channel.send(generatedMessage);
+        };
+    }
+
+    async onDiscordReady(guild, channels, roles) {
+        this.#logger.log('info', 'Channels loaded. Roles module is loading...');
+
+        this.#discordGuild = guild;
+        this.#discordChannels = channels;
+        this.#discordRoles = roles;
+
+        //this function should be moved in a singular class so it can also be called from a command so you dont have to restart the bot after adding or removing roles.
+        for (const role of this.#roles) {
+            this.#createOrUpdateMessage(role);
+        }
     }
 
     async onDiscordInteraction(interaction) {
@@ -116,50 +86,117 @@ export const RolesModule = class {
 
         if (action !== 'roleselection') return;
 
-        this.logger.log('debug', `Received button press from user ${interaction.user.tag} ${interaction.customId}`);
+        this.#logger.log('debug', `Received button press from user ${interaction.user.tag} ${interaction.customId}`);
 
-        const role = this.roles.find((role) => role.id === Number(roleId));
+        const role = this.#roles.find((role) => role.id === Number(roleId));
 
         if (!role) {
-            this.logger.log('warning', `Received button press for unknown role: ${roleId} from user: ${interaction.user.tag}`);
+            this.#logger.log('warning', `Received button press for unknown role: ${roleId} from user: ${interaction.user.tag}`);
             return;
         }
 
         const option = role.options[optionIndex];
 
         if (!option) {
-            this.logger.log('warning', `Received button press for unknown option: ${optionIndex} from user: ${interaction.user.tag}`);
+            this.#logger.log('warning', `Received button press for unknown option: ${optionIndex} from user: ${interaction.user.tag}`);
             return;
         }
 
-        this.logger.log('info', `Received button press from user ${interaction.user.tag} for role '${role.title}' option '${option.description}'`);
-
-        // Reply that it is okay
-        // TODO: Can we do a reply without any content? This is a workaround to avoid the "This interaction failed" message
-        interaction.reply({ content: 'OK', fetchReply: true })
-            .then((message) => message.delete())
-            .catch(console.error);
+        this.#logger.log('info', `Received button press from user ${interaction.user.tag} for role '${role.title}' option '${option.description}'`);
 
         // Check if the user has the role
         const member = interaction.member;
-        const discordRole = this.discordRoles.find((discordRole) => discordRole.name === option.role_name);
+        const discordRole = this.#discordRoles.find((discordRole) => discordRole.name === option.role_name);
+
+        //Check if role exists in guild
+        if(!discordRole){
+            this.#logger.log('error', `Role '${option.role_name}' does not exist in current guild.`);
+            return;
+        }
+
+        var roleAction = "";
 
         if (member.roles.cache.find((memberRole) => memberRole.id === discordRole.id)) {
             // Only remove if toggle is set to false.
             // This is used by the accept rules button which should not be removed.
             if (option.toggle !== false) {
-                this.logger.log('info', `User ${interaction.user.tag} already has role '${option.role_name}'. Removing...`);
+                this.#logger.log('info', `User ${interaction.user.tag} already has role '${option.role_name}'. Removing...`);
                 member.roles.remove(discordRole);
+                roleAction = 'Revoke'
             } else {
-                this.logger.log('info', `User ${interaction.user.tag} already has role '${option.role_name}'. Ignoring due to toggle=false`);
+                this.#logger.log('info', `User ${interaction.user.tag} already has role '${option.role_name}'. Ignoring due to toggle=false`);
             }
         } else {
-            this.logger.log('info', `User ${interaction.user.tag} does not have role '${option.role_name}'. Adding...`);
+            this.#logger.log('info', `User ${interaction.user.tag} does not have role '${option.role_name}'. Adding...`);
             member.roles.add(discordRole);
+            roleAction = 'Assigned'
         }
+
+        interaction.reply({
+            embeds: [{
+                title: `${roleAction} role`,
+                description: `${option.role_name}`
+             }],
+             ephemeral: true
+        });
+    }
+};
+
+const ButtonEmbedGenerator = class {
+    #embed
+    #rows
+
+    constructor(role){
+        let buttons = [];
+
+        for (let i = 0; i < role.options.length; i++) {
+            this.#generateButton(role, role.options[i], i, buttons);
+        }
+
+        // Split up rows if there are more than 5 buttons
+        this.#rows = [];
+        while (buttons.length > 0) {
+            this.#rows.push(new ActionRowBuilder()
+                .addComponents(buttons.splice(0, 5))
+            );
+        }
+
+        this.#embed = new EmbedBuilder()
+            .setTitle(role.title)
+            .setDescription(" ")
+            .setColor(0x0099FF);
+
+        var fieldDescription = "";
+        for (const option of role.options) {
+            fieldDescription += option.description + "\n";
+        }
+
+        this.#embed.addFields({
+            name: '\u200B',
+            value: fieldDescription,
+            inline: false
+        });
     }
 
-    getButtonStyleFromString(style) {
+    #generateButton(role, option, i, buttons) {
+        var button = new ButtonBuilder()
+            .setCustomId('roleselection_' + role.id + '_' + i)
+            .setStyle(this.#getButtonStyleFromString(option.button_style));
+
+        if (option.button_text) {
+            button.setLabel(option.button_text);
+        } else {
+            button.setLabel('\u200B');
+        }
+
+        if (option.button_emoji) {
+            button.setEmoji(option.button_emoji);
+        }
+
+        buttons.push(button);
+    }
+
+    #getButtonStyleFromString(style) {
         switch (style) {
             case 'Primary':
                 return ButtonStyle.Primary;
@@ -175,4 +212,12 @@ export const RolesModule = class {
                 return ButtonStyle.Secondary;
         }
     }
-};
+
+    getEmbed(){
+        return this.#embed;
+    }
+
+    getRows() {
+        return this.#rows;
+    }
+}
