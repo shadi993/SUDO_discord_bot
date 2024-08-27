f8d404e92922c462aeec4b55ef5dc1747e9754fd
Robin Degen <robindegen@gmail.com>
2024-08-27 15:38:54 +0200

    Fix config not loading config.json correctly

--- src/core/config.mjs
+++ src/core/config.mjs
@@ -3,7 +3,7 @@ import * as fs from 'node:fs';
 export var Config;
 
 export const InitConfig = () => {
-    Config = JSON.parse(fs.readFileSync('roles.json'));
+    Config = JSON.parse(fs.readFileSync('config.json'));
 
     if (!Config) {
         throw new Error('Config must be an object.');
